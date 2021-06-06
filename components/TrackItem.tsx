94461f9324fcb4f63a48ef9bbb96cd36190118a5
Alexandr <alex.develop777@gmail.com>
2021-06-06 12:40:18 +0300

    TrackItem style

--- components/TrackItem.tsx
+++ components/TrackItem.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {ITrack} from "../types/tracks";
-import {Card} from "@material-ui/core";
+import {Card, Grid, IconButton} from "@material-ui/core";
 import styles from '../styles/TrackItem.module.scss';
+import {Pause, PlayArrow, Delete} from "@material-ui/icons";
 
 interface TrackItemProps {
     track: ITrack;
@@ -11,7 +12,18 @@ interface TrackItemProps {
 const TrackItem: React.FC<TrackItemProps> = ({track, active = false}) => {
     return (
         <Card className={styles.track}>
-            {track.name}
+            <IconButton>
+                {active ? <Pause/> : <PlayArrow/>}
+            </IconButton>
+            <img width={70} height={70} src={track.picture}/>
+            <Grid container direction={"column"} className={styles.track__text}>
+                <div>{track.name}</div>
+                <div className={styles.track__artist}>{track.artist}</div>
+            </Grid>
+            {active && <div>02:42 / 03:22</div>}
+            <IconButton className={styles.track__icoDelete}>
+                <Delete/>
+            </IconButton>
         </Card>
     );
 };
