c5cf455d659503bca8ee1a42ebecd7fe9ccabca4
Alexandr <alex.develop777@gmail.com>
2021-06-06 09:41:07 +0300

    Create

--- pages/tracks/index.tsx
+++ pages/tracks/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import MainLayout from "../../layout/MainLayout";
+import {Box, Button, Card, Grid} from "@material-ui/core";
+import {useRouter} from "next/router";
 
 const Tracks = () => {
+    const router = useRouter();
+
     return (
         <MainLayout>
             <div>
-                Список треков
+                <Grid container justifyContent={"center"}>
+                    <Card style={{width: 900}}>
+                        <Box p={3}>
+                            <Grid container justifyContent={"space-between"}>
+                                <h1>Список треков</h1>
+                                <Button onClick={() => router.push('/tracks/create')}>Загрузить</Button>
+                            </Grid>
+                        </Box>
+                    </Card>
+                </Grid>
             </div>
         </MainLayout>
     );
