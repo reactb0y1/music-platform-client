169de1ff6650f83db27b1764065a29ae9b34270a
Alexandr <alex.develop777@gmail.com>
2021-06-06 16:01:19 +0300

    StepWrapper, create

--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainLayout from "../../layout/MainLayout";
+import StepWrapper from "../../components/StepWrapper";
+import {Button, Grid} from "@material-ui/core";
 
 const Create = () => {
+    const [activeStep, setActiveStep] = useState(0);
+    const back = () => setActiveStep(prev => prev - 1);
+    const next = () => setActiveStep(prev => prev + 1);
+
     return (
         <MainLayout>
-            <h1>Загрузка треков</h1>
+            <StepWrapper activeStep={activeStep}>
+                {activeStep === 0 &&
+                    <h1>STEP 1</h1>
+                }
+                {activeStep === 1 &&
+                    <h1>STEP 2</h1>
+                }
+                {activeStep === 2 &&
+                    <h1>STEP 3</h1>
+                }
+            </StepWrapper>
+            <Grid container justifyContent={"space-between"}>
+                <Button onClick={back} disabled={activeStep === 0}>Назад</Button>
+                <Button onClick={next} disabled={activeStep === 2}>Далее</Button>
+            </Grid>
         </MainLayout>
     );
 };
